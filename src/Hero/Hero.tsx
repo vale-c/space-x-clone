268210a9a8a343faf9616c083451ee9d953e71a8
Valentina Calabrese <valentina.calabrese@remote.com>
2023-07-22 14:29:44 +0200

    adjusting padding

--- src/Hero/Hero.tsx
+++ src/Hero/Hero.tsx
@@ -1,32 +1,32 @@
-import { ReactComponent as Chevron } from "../assets/chevron.svg";
-import hero1 from "../assets/hero-1.jpg";
-import hero3 from "../assets/hero-3.jpeg";
-import hero4 from "../assets/hero-4.jpeg";
+import { ReactComponent as Chevron } from '../assets/chevron.svg';
+import hero1 from '../assets/hero-1.jpg';
+import hero3 from '../assets/hero-3.jpeg';
+import hero4 from '../assets/hero-4.jpeg';
 
 type HeroCard = {
   header: string;
   image: string;
   link: string;
-  launchType?: "upcoming" | "recent";
+  launchType?: 'upcoming' | 'recent';
 };
 
 const heroCards: HeroCard[] = [
   {
-    header: "starlink mission",
+    header: 'starlink mission',
     image: hero1,
-    link: "https://www.spacex.com/launches/mission/?missionId=sl-6-6",
-    launchType: "upcoming",
+    link: 'https://www.spacex.com/launches/mission/?missionId=sl-6-6',
+    launchType: 'upcoming',
   },
   {
-    header: "esa euclid mission",
+    header: 'esa euclid mission',
     image: hero3,
-    link: "https://www.spacex.com/launches/mission/?missionId=euclid",
-    launchType: "recent",
+    link: 'https://www.spacex.com/launches/mission/?missionId=euclid',
+    launchType: 'recent',
   },
   {
-    header: "starship flight test",
+    header: 'starship flight test',
     image: hero4,
-    link: "https://www.spacex.com/launches/mission/?missionId=starship-flight-test",
+    link: 'https://www.spacex.com/launches/mission/?missionId=starship-flight-test',
   },
 ];
 
@@ -41,7 +41,7 @@ export const Hero = () => {
               alt="hero"
               className="w-full object-cover min-h-[850px]"
             />
-            <section className="absolute bottom-36 left-16 xl:left-52 drop-shadow-lg">
+            <section className="absolute bottom-36 left-16 xl:left-40 drop-shadow-lg">
               {card.launchType && (
                 <h4 className="mb-2 text-xl uppercase font-light tracking-tight text-white">
                   {card.launchType} launch
@@ -54,7 +54,7 @@ export const Hero = () => {
                 href={card.link}
                 className="bg-gradient-to-t from-transparent to-transparent border-2 text-white text-xs font-semibold px-16 py-5 uppercase tracking-tight transition-all duration-500 ease-in-out hover:text-black hover:from-white hover:to-white"
               >
-                {card.launchType === "upcoming" ? "watch" : "rewatch"}
+                {card.launchType === 'upcoming' ? 'watch' : 'rewatch'}
               </a>
             </section>
             <Chevron className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-pulse fill-transparent" />
--- src/Navigation/Menu.tsx
+++ src/Navigation/Menu.tsx
@@ -1,5 +1,5 @@
-import { ReactComponent as SpaceXLogo } from "../assets/spacex.svg";
-import { useEffect, useRef, useState } from "react";
+import { ReactComponent as SpaceXLogo } from '../assets/spacex.svg';
+import { useEffect, useRef, useState } from 'react';
 
 type MenuItem = {
   href: string;
@@ -7,28 +7,28 @@ type MenuItem = {
 };
 
 const TopBarMenuItems: MenuItem[] = [
-  { href: "https://www.spacex.com/vehicles/falcon-9/", title: "falcon 8" },
+  { href: 'https://www.spacex.com/vehicles/falcon-9/', title: 'falcon 8' },
   {
-    href: "https://www.spacex.com/vehicles/falcon-heavy/",
-    title: "falcon heavy",
+    href: 'https://www.spacex.com/vehicles/falcon-heavy/',
+    title: 'falcon heavy',
   },
-  { href: "https://www.spacex.com/vehicles/dragon/", title: "dragon" },
-  { href: "https://www.spacex.com/vehicles/starship/", title: "starship" },
+  { href: 'https://www.spacex.com/vehicles/dragon/', title: 'dragon' },
+  { href: 'https://www.spacex.com/vehicles/starship/', title: 'starship' },
   {
-    href: "https://www.spacex.com/human-spaceflight/",
-    title: "human spaceflight",
+    href: 'https://www.spacex.com/human-spaceflight/',
+    title: 'human spaceflight',
   },
-  { href: "https://www.spacex.com/rideshare/", title: "rideshare" },
-  { href: "https://www.spacex.com/starshield/", title: "starshield" },
-  { href: "https://www.starlink.com/", title: "starlink" },
+  { href: 'https://www.spacex.com/rideshare/', title: 'rideshare' },
+  { href: 'https://www.spacex.com/starshield/', title: 'starshield' },
+  { href: 'https://www.starlink.com/', title: 'starlink' },
 ];
 
 const SideBarMenuItems: MenuItem[] = [
-  { href: "https://www.spacex.com/mission/", title: "mission" },
-  { href: "https://www.spacex.com/launches/", title: "launches" },
-  { href: "https://www.spacex.com/careers/", title: "careers" },
-  { href: "https://www.spacex.com/updates/", title: "updates" },
-  { href: "https://shop.spacex.com/", title: "shop" },
+  { href: 'https://www.spacex.com/mission/', title: 'mission' },
+  { href: 'https://www.spacex.com/launches/', title: 'launches' },
+  { href: 'https://www.spacex.com/careers/', title: 'careers' },
+  { href: 'https://www.spacex.com/updates/', title: 'updates' },
+  { href: 'https://shop.spacex.com/', title: 'shop' },
 ];
 
 export const Menu = () => {
@@ -48,19 +48,19 @@ export const Menu = () => {
 
   useEffect(() => {
     if (isOpen) {
-      document.addEventListener("mousedown", handleClickOutside);
+      document.addEventListener('mousedown', handleClickOutside);
     } else {
-      document.removeEventListener("mousedown", handleClickOutside);
+      document.removeEventListener('mousedown', handleClickOutside);
     }
 
     return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
+      document.removeEventListener('mousedown', handleClickOutside);
     };
   }, [isOpen]);
 
   return (
     <>
-      <nav className="py-0 lg:py-6 justify-between lg:flex break-keep px-16 justify-between items-center xl:px-52 absolute w-full z-10 mix-blend-lighten">
+      <nav className="py-0 lg:py-6 justify-between lg:flex break-keep px-16 justify-between items-center xl:px-40 absolute w-full z-10 mix-blend-lighten">
         <div className="flex">
           <SpaceXLogo className="hidden lg:block w-56 h-auto mr-12 " />
           <ul className="hidden lg:flex mt-4 space-x-8 font-bold text-xs text-white tracking-tight">
@@ -85,19 +85,19 @@ export const Menu = () => {
           </a>
           <SpaceXLogo className="lg:hidden w-36" />
           <button onClick={handleToggle} className="text-white text-2xl ml-8">
-            {isOpen ? "✕" : "☰"}
+            {isOpen ? '✕' : '☰'}
           </button>
         </div>
       </nav>
       <aside
         ref={node}
         className={`transform top-0 right-0 w-64 bg-black fixed h-full overflow-auto ease-in-out transition-all duration-300 z-30 ${
-          isOpen ? "translate-x-0" : "translate-x-full"
+          isOpen ? 'translate-x-0' : 'translate-x-full'
         }`}
       >
         <div className="text-white p-5">
           <button onClick={handleToggle} className="text-xl float-right">
-            {isOpen ? "✕" : ""}
+            {isOpen ? '✕' : ''}
           </button>
           <ul className="space-y-4 text-right mt-12">
             {SideBarMenuItems.map((item: MenuItem) => (
